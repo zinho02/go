2cecb0db7c5f2745800a0861cdb7fee31deeddae
Matheus de Oliveira Saldanha <matheus-oliveira02@hotmail.com>
2021-07-25 22:58:31 -0300

    Implement pqc package

---
+++ src/crypto/pqc/pqc_test.go
@@ -0,0 +1,59 @@
+package pqc_test
+
+import (
+	"std/crypto/pqc"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var alg string = "dilithium5"
+
+var msg string = "Sign this."
+
+func generateKey() (*pqc.PrivateKey, error) {
+	return pqc.GenerateKey(alg)
+}
+
+func TestGenerateKey(t *testing.T) {
+	privKey, _ := pqc.GenerateKey(alg)
+	assert.Equal(t, alg, privKey.AlgName)
+}
+
+func TestPrivateKeyEqual(t *testing.T) {
+	privKey, _ := generateKey()
+	assert.True(t, privKey.Equal(privKey))
+}
+
+func TestPublicKeyEqual(t *testing.T) {
+	privKey, _ := generateKey()
+	pubKey := privKey.PQCPublic()
+	assert.True(t, pubKey.Equal(pubKey))
+}
+
+func TestPublic(t *testing.T) {
+	privKey, _ := generateKey()
+	pubKey := privKey.PQCPublic()
+	assert.Equal(t, alg, pubKey.AlgName)
+	assert.Equal(t, privKey.Bytes, pubKey.Bytes)
+}
+
+func TestPQCPublic(t *testing.T) {
+	privKey, _ := generateKey()
+	pubKey := privKey.PQCPublic()
+	pqcPubKey := new(pqc.PublicKey)
+	assert.IsType(t, pqcPubKey, pubKey)
+}
+
+func TestSign(t *testing.T) {
+	privKey, _ := generateKey()
+	sig, _ := privKey.Sign(nil, []byte(msg), nil)
+	assert.NotNil(t, sig)
+}
+
+func TestVerify(t *testing.T) {
+	privKey, _ := generateKey()
+	sig, _ := privKey.Sign(nil, []byte(msg), nil)
+	verifyTest := pqc.Verify([]byte(msg), sig, privKey.PQCPublic(), privKey.AlgName)
+	assert.True(t, verifyTest)
+}
--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,8 @@ module std
 go 1.17
 
 require (
+	github.com/open-quantum-safe/liboqs-go v0.0.0-20210711155910-e7325d463abc
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210503195802-e9a32991a82e
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
